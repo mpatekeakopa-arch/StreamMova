2543ac409b259b1470b069479ff78cfc68879d01
mpatekeakopa-arch <mpatekeakopa@gmail.com>
2026-03-02 18:08:56 +0200

    Add files via upload

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,13 @@
+// src/lib/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
